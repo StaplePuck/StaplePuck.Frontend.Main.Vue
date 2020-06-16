beb4865d4ec2cc197203bbc87b6c8a2dc6beaad0
nickhetland <nick.hetland@infiniteleap.net>
2020-06-15 22:45:13 -0500

    Group by season

--- src/components/AllLeagues.vue
+++ src/components/AllLeagues.vue
@@ -3,18 +3,25 @@
     <h4 v-if="loading">Loading...</h4>
     <div v-else>
       <h1>{{ msg }}</h1>
-      <div class="card-deck">
-        <div class="card" v-for="(league, idx) in leagues" :key="idx">
-          <div class="card-header">
-            <router-link :to="{ name: 'league', params: { id: league.id } }"
-              >{{ league.name }}
-            </router-link>
-          </div>
-          <div class="card-body">
-            <p>{{ league.season.sport.name }}</p>
-            <p>{{ league.season.fullName }}</p>
-            <p>{{ league.description }}</p>
-            <p style="color: red;">{{ league.announcement }}</p>
+      <div
+        v-for="(season, ids) in seasons.slice().reverse()"
+        track-by="id"
+        :key="ids"
+      >
+        <h1>{{ season.fullName }}</h1>
+        <div class="card-deck">
+          <div class="card" v-for="(league, idx) in season.leagues" :key="idx">
+            <div class="card-header">
+              <router-link :to="{ name: 'league', params: { id: league.id } }"
+                >{{ league.name }}
+              </router-link>
+            </div>
+            <div class="card-body">
+              <p>{{ league.season.sport.name }}</p>
+              <p>{{ league.season.fullName }}</p>
+              <p>{{ league.description }}</p>
+              <p style="color: red;">{{ league.announcement }}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -32,12 +39,12 @@ export default {
   },
   data() {
     return {
-      leagues: [],
+      seasons: [],
       loading: 0
     };
   },
   apollo: {
-    leagues: {
+    seasons: {
       query: QUERY_ALL_LEAGUES
     }
   }
