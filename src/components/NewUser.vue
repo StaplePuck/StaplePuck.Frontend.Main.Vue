a1eee1a159ae18a2afcc1f0e4413290172fa8346
Jacob Distel <jacob.distel@gmail.com>
2020-06-17 07:43:07 -0400

    Removed unused BootstrapVue. Updated new user form

--- src/components/NewUser.vue
+++ src/components/NewUser.vue
@@ -6,80 +6,75 @@
       Note that the handle is not the same as your team name.
     </p>
     <h4 v-if="loading">Loading...</h4>
-    <b-form v-else @submit="updateUser">
-      <div class="text-left">
-        <b-form-group
-          label-cols-sm="3"
-          label="Handle:"
-          label-align-sm="right"
-          label-for="userName"
+    <form v-else @submit="updateUser">
+      <div class="form-group">
+        <label label-for="userName">Handle:</label>
+        <input
+          type="text"
+          id="userName"
+          class="form-control"
+          required
+          trim
+          v-model="currentUser.name"
+        />
+      </div>
+      <div class="form-group">
+        <label label-for="userEmail">Email Address:</label>
+        <input
+          type="text"
+          id="userEmail"
+          class="form-control"
+          required
+          trim
+          v-model="currentUser.email"
+        />
+      </div>
+      <label>Receive Emails:</label>
+      <div role="radiogroup" tabindex="-1">
+        <div
+          class="form-check form-check-inline"
+          v-for="(emailOption, key) in emailOptions"
+          :key="key"
         >
-          <b-input
-            id="userName"
-            v-model="currentUser.name"
-            required
-            trim
-          ></b-input>
-        </b-form-group>
-        <b-form-group
-          label-cols-sm="3"
-          label="Email Address:"
-          label-align-sm="right"
-          label-for="userEmail"
-        >
-          <b-input
-            id="userEmail"
-            v-model="currentUser.email"
-            required
-            trim
-          ></b-input>
-        </b-form-group>
-        <b-form-group
-          label-cols-sm="3"
-          label="Receive Emails:"
-          label-align-sm="right"
-          class="mb-0"
-          label-for="userReceiveEmails"
-        >
-          <b-form-radio-group
-            class="pt-2"
-            id="userReceiveEmails"
+          <input
+            type="radio"
+            class="form-check-input"
             name="receiveEmails"
+            :id="'userReceiveEmails_' + key"
+            :value="key"
             v-model="currentUser.receiveEmails"
-          >
-            <b-form-radio value="true">Yes</b-form-radio>
-            <b-form-radio value="false">No</b-form-radio>
-          </b-form-radio-group>
-        </b-form-group>
-        <div v-show="saveSuccess" class="alert alert-success">
-          Save Successful
-        </div>
-        <div v-show="saveFailed" class="alert alert-danger">
-          <p>User data not saved:</p>
-          <p
-            v-for="(thisError, index) in saveErrors"
-            :key="`saveErrors-${index}`"
-          >
-            {{ thisError }}
-          </p>
-        </div>
-        <div class="text-center">
-          <button
-            class="btn btn-secondary a-button"
-            type="submit"
-            :disabled="saving == 1"
-          >
-            <span
-              class="spinner-border spinner-border-sm"
-              role="status"
-              aria-hidden="true"
-              v-if="saving == 1"
-            ></span>
-            Save
-          </button>
+          />
+          <label class="form-check-label" for="'userReceiveEmails_' + key">
+            {{ emailOption }}
+          </label>
         </div>
       </div>
-    </b-form>
+      <div v-show="saveSuccess" class="alert alert-success">
+        Save Successful
+      </div>
+      <div v-show="saveFailed" class="alert alert-danger">
+        <p>User data not saved:</p>
+        <p
+          v-for="(thisError, index) in saveErrors"
+          :key="`saveErrors-${index}`"
+        >
+          {{ thisError }}
+        </p>
+      </div>
+      <button
+        class="btn btn-secondary a-button"
+        type="submit"
+        :disabled="saving == 1"
+      >
+        <span
+          class="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"
+          v-if="saving == 1"
+        ></span>
+        Save
+      </button>
+    </form>
   </div>
 </template>
 
@@ -94,6 +89,7 @@ export default {
       currentUser: { name: "", email: "", receiveEmails: true },
       newUser: false,
       loading: 0,
+      emailOptions: { true: "Yes", false: "No" },
       saving: 0,
       saveSuccess: false,
       saveFailed: false,
--- src/main.js
+++ src/main.js
@@ -8,47 +8,21 @@ import "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 
 import {
-  BCardGroup,
-  BCard,
-  BCardBody,
-  BCardText,
-  BCardHeader,
-  BListGroup,
-  BListGroupItem,
-  BRow,
-  BCol,
   BForm,
   BFormGroup,
   BFormInput,
-  BFormRadioGroup,
-  BFormRadio,
   BButton,
   BTable,
-  BFormSelect,
-  ToastPlugin,
   BFormCheckbox
 } from "bootstrap-vue";
 
 Vue.config.productionTip = false;
-Vue.component("b-card-group", BCardGroup);
-Vue.component("b-card", BCard);
-Vue.component("b-card-body", BCardBody);
-Vue.component("b-card-text", BCardText);
-Vue.component("b-card-header", BCardHeader);
-Vue.component("b-list-group", BListGroup);
-Vue.component("b-list-group-item", BListGroupItem);
-Vue.component("b-row", BRow);
-Vue.component("b-col", BCol);
 Vue.component("b-form", BForm);
 Vue.component("b-form-group", BFormGroup);
 Vue.component("b-input", BFormInput);
-Vue.component("b-form-radio-group", BFormRadioGroup);
-Vue.component("b-form-radio", BFormRadio);
-Vue.component("b-form-select", BFormSelect);
 Vue.component("b-form-checkbox", BFormCheckbox);
 Vue.component("b-button", BButton);
 Vue.component("b-table", BTable);
-Vue.use(ToastPlugin);
 
 Vue.use(Auth0Plugin, {
   onRedirectCallback: async (appState) => {
