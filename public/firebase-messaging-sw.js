b432e985f3292ad2e673ea1bc5995afe58c009d6
nickhetland <nicholas.hetland@gmail.com>
2023-03-04 21:59:52 -0600

    Update imports

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js"
-);
-importScripts("./env-vars.js");
+importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js");
 
 var config = {
   apiKey: VUE_APP_FIREBASE_API_KEY,
